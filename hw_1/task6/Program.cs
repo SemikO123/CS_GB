19b251cf0fafa2c3e43c5a494b56cc3da0d4c65e
Yoda <semik_dom@mail.ru>
2023-10-18 22:22:03 +0300

    task 6

--- hw_1/task6/Program.cs
+++ hw_1/task6/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдает, 
+// является ли число четным (делится ли оно на два без остатка).
+
+Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0) Console.WriteLine("Yes");
+else Console.WriteLine("No");
