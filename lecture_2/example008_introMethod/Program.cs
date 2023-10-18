26a81fd946be0ef2076d4151ae8b249aa8336bb8
Yoda <semik_dom@mail.ru>
2023-10-18 23:18:35 +0300

    added examples 8 and 9

---
+++ lecture_2/example008_introMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿int Max(int a, int b, int c)
+{
+  int max = a;
+  if (b > max) max = b;
+  if (c > max) max = c;
+  return max;
+}
+
+
+int a1 = 15, b1 = 21, c1 = 39,
+    a2 = 13, b2 = 14, c2 = 233,
+    a3 = 44, b3 = 1000, c3 = 45;
+
+int max1 = Max(a1, b1, c1),
+    max2 = Max(a2, b2, c2),
+    max3 = Max(a3, b3, c3);
+
+int superMax = Max(max1, max2, max3);
+//int superMax = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine("Max = " + superMax);
---
+++ lecture_2/example009_introarray/Program.cs
@@ -0,0 +1,26 @@
+﻿// int Max(int a, int b, int c)
+// {
+//   int max = a;
+//   if (b > max) max = b;
+//   if (c > max) max = c;
+//   return max;
+// }
+
+// int max1 = Max(arr[0], arr[1], arr[2]),
+//     max2 = Max(arr[3], arr[4], arr[5]),
+//     max3 = Max(arr[6], arr[7], arr[8]);
+
+// int superMax = Max(max1, max2, max3);
+// Console.WriteLine("Max = " + superMax);
+
+int[] arr = { 1, 2, 223, 4, 487, 6, 7, 8, 9 };
+
+int max = arr[0], i = 0;
+while (i < arr.Length)
+{
+  if (arr[i] > max) max = arr[i];
+  i++;
+}
+
+Console.WriteLine("max = " + max);
+
