8fede9ea45d42c8a7c2cedb487f5612a3e260b15
Yoda <semik_dom@mail.ru>
2023-10-18 23:46:16 +0300

    added examples 10 & 11

---
+++ lecture_2/example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = { 1, 2, 31, 4, 56, 34, 54, 66, 4 };
+
+int n = arr.Length;
+
+int find = 4;
+int i = 0;
+while (i < n)
+{
+  if (arr[i] == find)
+  {
+    Console.WriteLine(i);
+    break; //прерывыает цикл
+  }
+  i++;
+}
---
+++ lecture_2/example011_ArrayLibrary/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillArray(int[] collection)
+{
+  int length = collection.Length;
+  int i = 0;
+  while (i < length)
+  {
+    collection[i] = new Random().Next(1, 10);
+    i++;
+  }
+}
+
+void PrintArray(int[] collection)
+{
+  int length = collection.Length;
+  int i = 0;
+  while (i < length)
+  {
+    Console.Write(collection[i++] + " ");
+  }
+  Console.WriteLine();
+}
+
+int IndexOf(int[] collection, int find)
+{
+  int count = collection.Length;
+  int i = 0;
+
+  while(i < count)
+  {
+    if (collection[i] == find)
+    {
+      return i;
+    }
+    i++;
+  }
+  return -1;
+}
+
+
+int[] arr = new int[10];
+FillArray(arr);
+PrintArray(arr);
+int find = 4;
+int pos = IndexOf(arr, find);
+
+Console.WriteLine("Position of " + find + " is " + pos);
