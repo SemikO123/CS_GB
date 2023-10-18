5793564ca2495837aa46a3a56ee6a82593602abb
Yoda <semik_dom@mail.ru>
2023-10-18 22:19:28 +0300

    task 4

--- hw_1/task4/Program.cs
+++ hw_1/task4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+
+Console.Write("Input 1st number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 2nd number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 3rd number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("max = " + max);
