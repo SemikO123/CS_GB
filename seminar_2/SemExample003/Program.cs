07ece51b329cb705e01030ead33bedacf74ed2d3
Yoda <semik_dom@mail.ru>
2023-10-19 16:26:13 +0300

    added seminar 2 example004

--- seminar_2/SemExample003/Program.cs
+++ seminar_2/SemExample003/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Input number : ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 7 == 0 && num1 % 23 == 0) Console.WriteLine("yes");
+else Console.WriteLine("No");
---
+++ seminar_2/SemExample004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Write("Input 1st number : ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input 2nd number : ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 * num1 == num2 || num1 == num2 * num2)
+{
+  Console.WriteLine("yes");
+}
+else System.Console.WriteLine("no");
