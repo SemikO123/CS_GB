19bfaf902a024339f36252e698cb348a3c41a10b
Yoda <semik_dom@mail.ru>
2023-10-19 15:01:19 +0300

    added seminar 2 examples 0 1 2 3

---
+++ seminar_2/SemExample000/Program.cs
@@ -0,0 +1,8 @@
+﻿int num = new Random().Next(10, 100);
+
+int dig1 = num / 10,
+    dig2 = num % 10;
+
+int max = dig1 > dig2 ? dig1: dig2;
+
+Console.WriteLine("Max digit from number " + num + " is " + max);
---
+++ seminar_2/SemExample002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли Первое кратно второму. Если число 1 не кратно числу 2, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Input 1st number : ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input 2nd number : ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int mod = num1 % num2;
+
+Console.WriteLine(mod == 0 ? "Кратно" : ("Не кратно, остаток " + mod));
+
