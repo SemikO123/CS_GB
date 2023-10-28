615aee1ef54b03606d48f7fa332e2c38622821d6
Yoda <semik_dom@mail.ru>
2023-10-28 12:11:13 +0300

    added  seminar5 examples

---
+++ seminar_5/SemExample013/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+int[] GetArray(int len, int a, int b)
+{
+  int[] array = new int[len];
+  b++; // Генерировать будем до b включительно
+  Random rand = new Random();
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = rand.Next(a, b);
+  }
+  return array;
+}
+
+void PrintArray(int[] array)
+{
+  foreach (int num in array)
+  {
+    System.Console.Write($"{num} ");
+  }
+  System.Console.WriteLine();
+}
+
+// int GetSumOfPositive(int[] array)
+// {
+//   int sum = 0;
+//   foreach (int elem in array)
+//     if (elem > 0) sum += elem;
+//   return sum;
+// }
+
+// int GetSumOfNegative(int[] array)
+// {
+//   int sum = 0;
+//   foreach (int elem in array)
+//     if (elem < 0) sum += elem;
+//   return sum;
+// }
+
+int[] arr = GetArray(12, -9, 9);
+PrintArray(arr);
+
+// System.Console.WriteLine($"Summ of positive = {GetSumOfPositive(arr)}\n"
+//                         + $"Summ of negative = {GetSumOfNegative(arr)}");
+
+
+// пробую возвращаемые параметры
+void GetSums(int[] array, out int sumP, out int sumN) 
+{
+  sumP = 0;
+  sumN = 0;
+  foreach (int elem in array)
+    if (elem < 0) sumN += elem;
+    else sumP += elem;
+}
+
+
+int sumP = 0, sumN = 0;
+GetSums(arr, out sumP, out sumN); 
+
+System.Console.WriteLine($"Summ of positive = {sumP}\n"
+                        + $"Summ of negative = {sumN}");
---
+++ seminar_5/SemExample017/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] GetArray(int len, int a, int b)
+{
+  int[] array = new int[len];
+  b++; // Генерировать будем до b включительно
+  Random rand = new Random();
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = rand.Next(a, b);
+  }
+  return array;
+}
+
+void PrintArray(int[] array)
+{
+  foreach (int num in array)
+  {
+    // System.Console.Write($"{num} ");
+    System.Console.Write("{0, 4}", num);
+  }
+  System.Console.WriteLine();
+}
+
+int[] GetProduct(int[] array)
+{
+  int len = array.Length / 2 + array.Length % 2;
+
+  int[] prod = new int[len];
+  int lastInd = array.Length - 1;
+  for (int i = 0; i < len; i++)
+  {
+    prod[i] = array[i] * array[lastInd - i];
+  }
+  if (array.Length % 2 == 1)
+    prod[len - 1] = array[len - 1];
+
+  return prod;
+}
+
+Console.Clear();
+
+int[] arr = GetArray(31, 1, 20);
+PrintArray(arr);
+// System.Console.WriteLine("Произведения элементов: ");
+PrintArray(GetProduct(arr));
+
