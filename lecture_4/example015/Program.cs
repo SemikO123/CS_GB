1bdefa6409e9a72b36decc6fba5d71ec514a4f83
Yoda <semik_dom@mail.ru>
2023-10-29 01:25:04 +0300

    added  example from lecture 4

---
+++ lecture_4/example015/Program.cs
@@ -0,0 +1,95 @@
+﻿// string[,] table = new string[2,5];
+// table[1, 2] = "слово";
+
+// void PrintMatrix(int[,] matrix)
+// {
+//   for (int i = 0; i < matrix.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//       System.Console.Write("{0, 2}", matrix[i, j]);
+//     }
+//     System.Console.WriteLine();
+//   }
+// }
+
+void PrintMatrixFancy(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      System.Console.Write("{0, 2}", matrix[i, j] == 1 ? "#" : ".");
+    }
+    System.Console.WriteLine();
+  }
+}
+
+bool ValidateCoord(int n, int length)
+{
+
+  return (n >= 0 && n < length);
+}
+
+void FillImage(int[,] image, int x, int y)
+{
+  //System.Console.WriteLine($"x = {x} y = {y}");
+  if (ValidateCoord(x, image.GetLength(0)) && ValidateCoord(y, image.GetLength(1)))
+  {
+    // System.Console.WriteLine($"x = {x} y = {y}");
+    if (image[x, y] == 0)
+    {
+      image[x, y]++;
+      FillImage(image, x, y - 1); //left
+      FillImage(image, x - 1, y); //top
+      FillImage(image, x, y + 1); //right
+      FillImage(image, x + 1, y); //down
+    }
+  }
+}
+
+// void FillMatrix(int[,] matrix)
+// {
+//   Random rand = new Random();
+//   for (int i = 0; i < matrix.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//       matrix[i,j] = rand.Next(0,10);
+//     }
+//   }
+// }
+
+// int[,] image = new int[5, 5];
+// FillMatrix(image);
+// PrintMatrix(image);
+
+int[,] image = new int[,]
+{
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+  {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+  {0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+  {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+  {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+  {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+};
+
+// Console.Clear();
+// PrintMatrix(image);
+PrintMatrixFancy(image);
+FillImage(image, 0, 0);
+PrintMatrixFancy(image);
